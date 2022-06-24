f61fd2a0e5e5d15b7b1d73b5baa1caecd9c18a09
Joel Ricaño Rodriguez <joel.ricanorodriguez@utxicotepec.edu.mx>
2022-06-24 13:46:36 -0500

    primer commit cliente socket

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
+//sockets
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+import { environment } from 'src/environments/environment';
+const config: SocketIoConfig=
+{
+  url: environment.wsUrl, options:{}
+
+};
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
+    SocketIoModule.forRoot(config),
     AppRoutingModule
   ],
   providers: [],
---
+++ src/app/services/websocket.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { Socket } from 'ngx-socket-io';
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WebsocketService {
+public socketStatus =false;
+  constructor(
+    private socket: Socket
+
+    ) {}
+
+    checkStatus(){
+      this.socket.on('connect', () =>
+      {
+console.log('conectado al servidor');
+this.socketStatus = true;
+      })
+
+        this.socket.on('disconnect', () =>
+        {
+  console.log('Desconectado al servidor');
+  this.socketStatus = false;
+        })
+
+}}
